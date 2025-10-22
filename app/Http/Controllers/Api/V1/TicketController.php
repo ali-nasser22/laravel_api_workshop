9c325604d6bbd438d9d555bc056185ea0fae796e
Ali Nasser <alinasser.t22@gmail.com>
2025-10-22 19:27:31 +0300

    designing response body for tickets

--- app/Http/Controllers/Api/V1/TicketController.php
+++ app/Http/Controllers/Api/V1/TicketController.php
@@ -4,21 +4,31 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\TicketRequest;
+use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
+use App\Traits\ApiResponses;
+use Symfony\Component\HttpFoundation\Response;
 
 class TicketController extends Controller
 {
+    use ApiResponses;
+
     public function index()
     {
-        return Ticket::all();
+        return TicketResource::collection(Ticket::paginate(10));
     }
 
     public function store(TicketRequest $request)
     {
     }
 
-    public function show(Ticket $ticket)
+    public function show($id) // I used $id and not Route Model Binding, so I can use my custom $this->error() response
     {
+        $ticket = Ticket::find($id);
+        if (!$ticket) {
+            return $this->error('Ticket not found', Response::HTTP_NOT_FOUND);
+        }
+        return TicketResource::make($ticket);
     }
 
     public function update(TicketRequest $request, Ticket $ticket)
---
+++ app/Http/Resources/V1/TicketResource.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use App\Models\Ticket;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin Ticket */
+class TicketResource extends JsonResource
+{
+    // public static $wrap = 'ticket'; default is 'data'
+
+    public function toArray(Request $request): array
+    {
+        // you should see JSON:API Design at https://jsonapi.org, I followed it to design this response
+        return [
+            'type' => 'ticket',
+            'id' => $this->id,
+            'attributes' => [
+                'title' => $this->title,
+                'description' => $this->description,
+                'status' => $this->status,
+                'createdAt' => $this->created_at,
+                'updatedAt' => $this->updated_at,
+            ],
+            'relationships' => [
+                'author' => [
+                    'data' => [
+                        'type' => 'user',
+                        'id' => $this->user_id
+                    ],
+                    'links' => [
+                        'self' => 'todo',
+                    ]
+                ]
+            ],
+            'links' => [
+                [
+                    'self' => route("tickets.show", $this->id),
+                ]
+            ]
+        ];
+    }
+}
